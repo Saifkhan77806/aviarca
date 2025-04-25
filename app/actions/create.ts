30506c615abbeb0a99edd00dcb38dbeb5ff001a0
saifkhan77806 <saifkhan77806@gmail.com>
2025-04-25 05:56:44 +0530

    minor already feature function and unique constraint to email  is added

--- app/actions/create.ts
+++ app/actions/create.ts
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { formSchema } from "@/schema";
 import * as z  from "zod";
 import bcryptjs from 'bcryptjs'
+import { getUserByEmail } from "@/data/user";
 
 
 export const create = async (values: z.infer<typeof formSchema>) =>{
@@ -14,6 +15,12 @@ export const create = async (values: z.infer<typeof formSchema>) =>{
     }
 
     const {username, email, password, phone, address, position, salary} = validateData.data;
+
+    const existingUser = await getUserByEmail(email)
+
+    if(existingUser){
+        return {error: "User already exists !"}
+    }
 
     const hashedPassword = await bcryptjs.hash(password, 10); 
 
---
+++ data/user.ts
@@ -0,0 +1,12 @@
+'use server'
+
+import { db } from "@/lib/db"
+
+export const getUserByEmail = async (email: string) =>{
+    try{
+        const user = await db.user.findUnique({where:{email}})
+        return user
+    }catch(err){
+        return null
+    }
+}
